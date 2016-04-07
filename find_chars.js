4a0e305dedabbe168e89801e85fcfaa7d2037702
Timo Taglieber <github@timotaglieber.de>
2016-04-07 16:17:11 +0200

    Implement find_chars_unique

--- find_chars.js
+++ find_chars.js
@@ -44,3 +44,27 @@ function find_chars(string1, string2) {
   
   return intersection;
 }
+
+/* Just in case we don't want duplicated characters. */
+function find_chars_unique(string1, string2) {
+  var i1, i2, char1, char2;
+  var string2_chars = {};
+  var intersection = '';
+  
+  // Map out string2 to allow constant-time property lookup
+  // for individual characters. Value will be set to false if already seen.
+  for (i2 = 0; i2 < string2.length; i2++) {
+    char2 = string2[i2];
+    string2_chars[char2] = true;
+  }
+  
+  for (i1 = 0; i1 < string1.length; i1++) {
+      char1 = string1[i1];
+      if (string2_chars.hasOwnProperty(char1) && string2_chars[char1]) {
+          intersection += char1;
+          string2_chars[char1] = false;
+      }
+  }
+  
+  return intersection;
+}
