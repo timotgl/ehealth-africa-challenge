7047825ddc31890784e466e2b422db1be30b29e1
Timo Taglieber <github@timotaglieber.de>
2016-04-10 17:27:24 +0200

    First draft of angular app using pouchdb

---
+++ angular-pouchdb-put/README.md
@@ -0,0 +1,4 @@
+# Angularjs app with automated tests for angular-pouchdb's put method
+
+## How to run
+ 1. `bower install`
---
+++ angular-pouchdb-put/app/app.js
@@ -0,0 +1,2 @@
+// PouchPutDemo module depends on pouchdb module!
+var pouchPutDemo = angular.module('PouchPutDemo', ['pouchdb']);
